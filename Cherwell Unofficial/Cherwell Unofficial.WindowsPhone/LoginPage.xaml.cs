708c235a2cc6b035f582ca5c64c76f34faadd7af
adameska <hettinger14@outlook.com>
2014-06-22 09:40:13 -0500

    Importing Files from local repository.
    Initial import

---
+++ Cherwell Unofficial/Cherwell Unofficial.WindowsPhone/LoginPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Trebuchet.API;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
+
+namespace Cherwell_Unofficial
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            this.InitializeComponent();
+
+            Color accentColor = (Color)Application.Current.Resources["PhoneAccentColor"];
+
+            //if we have username and password info then login automatically, otherwise fill in last username
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+
+        }
+
+        private void login_Click(object sender, RoutedEventArgs e)
+        {
+            //Try and connect to cherwell servers.
+
+            if (loginAutomatically.IsChecked ?? false)
+            {
+                //todo store username/password info
+            }
+            else
+            {
+                //todo delete any saved password info so we stop logging in automatically
+            }
+        }
+    }
+}
